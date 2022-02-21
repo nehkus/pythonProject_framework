7c69b3f22153f036adf7125f9a9d368b683dc3a6
Sukhi <sukhenv@gmail.com>
2022-02-22 01:29:18 +0530

    Added more switches to config properties file and used it in base test.

--- generic/base_test.py
+++ generic/base_test.py
@@ -4,39 +4,66 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-
-#driver---->local variable
-#self.driver--->global variable
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver import FirefoxOptions
+# driver---->local variable
+# self.driver--->global variable
 
 class BaseTest:
 
     @pytest.fixture(autouse=True)
     def open_close_app(self):
-        #create object of Properties class
+        # create object of Properties class
         p_file = Properties()
-        #open the properties file and load it
+
+        # open the properties file and load it
         p_file.load(open("config.properties"))
 
-        self.xl_path=p_file['XLPATH']
-        #open the browser
-        self.driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.xl_path = p_file['XLPATH']
 
-        #maximize the browser
+        # open the browser
+        use_grid=p_file['USEGRID']
+
+        if use_grid=='YES':
+            print("Using Selenium Grid")
+            grid_url=p_file['GRIDURL']
+            browser=p_file['BROWSER']
+            print("Browser is:", browser)
+            if browser=='chrome':
+                browser_option = ChromeOptions()
+            else:
+                browser_option = FirefoxOptions()
+            self.driver = webdriver.Remote(command_executor=grid_url, options=browser_option)
+
+        else:
+            print("Using Local System")
+            browser = p_file['BROWSER']
+            print("Browser = ", browser)
+            if browser=='chrome':
+                self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+            else:
+                self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+
+
+        # maximize the browser
         self.driver.maximize_window()
 
-        #implicit wait
-        ito=p_file['ITO'] #get ITO from properties file
+        # implicit wait
+        ito = p_file['ITO']  # get ITO from properties file
         self.driver.implicitly_wait(ito)
 
-        #explicit wait
-        eto=p_file['ETO'] #get ETO from properties file
-        self.wait=WebDriverWait(self.driver,eto)
+        # explicit wait
+        eto = p_file['ETO']  # get ETO from properties file
+        self.wait = WebDriverWait(self.driver, eto)
 
-        #enter the URL
-        url=p_file['URL'] #get URL from properties file
+        # enter the URL
+        url = p_file['URL']  # get URL from properties file
         self.driver.get(url)
 
-        yield #Go,run the test and come back
+        yield  # Go,run the test and come back
 
-        #close the browser
+        # close the browser
         self.driver.close()
